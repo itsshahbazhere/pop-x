39c3492779c4a14f586b8c087561e661f5d10505
itsshahbazhere <sa0418600@gmail.com>
2025-06-13 14:06:45 +0530

    prevent scroll behaviour commit

--- src/components/Width.jsx
+++ src/components/Width.jsx
@@ -3,9 +3,9 @@ const Width = ({ children, align = "top" }) => {
   const justify = align === "bottom" ? "justify-end" : "justify-top";
 
   return (
-    <div className="min-h-screen bg-white flex justify-center items-center p-4">
+    <div className="h-[100dvh] bg-white flex justify-center items-center p-4">
       <div
-        className={`w-[360px] bg-gray-50 border border-gray-300  min-h-[85vh] flex flex-col ${justify}`}
+        className={`w-[360px] bg-gray-50 border h-full border-gray-300  flex flex-col ${justify}`}
       >
         {children}
       </div>
